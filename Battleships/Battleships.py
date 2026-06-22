3b5f86e7259dbffbb0a785ba78b0610e24d89e87
Maliketh1729 <173165@lrgs.org.uk>
2026-06-22 15:30:02 +0100

    Added vertical ship gen
    Allowed particular ship classes to generate with a vertical instead of horizontal length

--- Battleships/Battleships.py
+++ Battleships/Battleships.py
@@ -8,6 +8,15 @@ clock = pygame.time.Clock()         #Limits FPS to 60
 
 #Much of this code was imported from my personal platformer project, so it may be a little messy
 
+pickleExists = os.path.exists(r"Z:\Yr 10 Computer Science\Password system\userPassword.pkl")
+if pickleExists == True:
+    with open('score.pkl', 'rb') as f:
+        highScore = pickle.load(f)
+        pickleConfirmation = True
+else:
+    print("No score file found - high score inaccessible")
+    pickleConfirmation = False
+
 #Apologies, it may require another module on top of pyGame - pySimpleGUI.
 #It can be installed through powershell with 'python -m pip install PySimpleGUI'
 
@@ -62,6 +71,7 @@ class Player(pygame.sprite.Sprite):
                 if pressed_keys[K_SPACE] and self.torpedos > 0: #And rect is pressed and torpedos remain:
                     print("Firing Shot!")                         #Fire
                     print("HIT!!!!!!")
+                    print("Ships left:", len(ship_list)-1)
                     self.torpedos -= 1
                     pygame.sprite.Sprite.kill(ship)               #Delete the hit ship
                     ship = []                                     #Clear the log of collisions
@@ -70,6 +80,7 @@ class Player(pygame.sprite.Sprite):
                 if pressed_keys[K_SPACE] and self.torpedos > 0: #And rect is pressed and torpedos remain:
                     print("Firing Shot!")                         #Fire
                     print("Miss")
+                    print("Ships left are stll:", len(ship_list)-1)
                     self.torpedos -= 1
                     miss = []                                     #Clear the log of collisions
 
@@ -138,30 +149,40 @@ class Empty(pygame.sprite.Sprite):
 #need to make empty space spawn in all non-occupied areas
 #maybye make a list of all allowed positions, allocate ships to it randomly and 0s become empty?
 
-horiPos = [0,128,256,384,512,640,768,896]
-vertPos = [0,128,256,384,512,640,768,896]
+horiPos = [0,128,256,384,512,640,768,896,1024,1152,1280]
+vertPos = [0,128,256,384,512,640,768,896,1024]
 #possLoc = [[horipos[0],vertpos[0]],[horipos[1],vertpos[0]...]      This would work but would be very inefficient to type
+
+#could store ship locations in a list and regenerate if there is any overlap?
 
 
 class Level:                                             
     def ship(level, tx, ty):
         ship_list = pygame.sprite.Group()
-        shloc = []   #sploc is a list which determines the location of the ships e.g. [0, 642,screenY,256]. it may be more efficient to add locs here.oly do when sure of a pos
+        hshloc = []
+        vshloc = []
         i = 0
         if level == 1:
              #Place spike locations for stage 1 here 
-             shloc.append((horiPos[random.randint(0,7)], vertPos[random.randint(0,7)],4))  #BATTLESHIP         #The last number is how many ships in a row there are                        
-             shloc.append((worldx // 2, worldy -ty, 0)) #CRUISER
-        elif level == 2:
-             shloc.append((worldx // 2+ty, worldy //2, 10))
-             shloc.append((worldx // 2, worldy -ty, 10))
-        while i < len(shloc):                                    
+             hshloc.append((horiPos[random.randint(0,7)], vertPos[random.randint(0,7)], 4))  #CARRIER         #The last number is how many ships in a row there are -1                       
+             hshloc.append((horiPos[random.randint(0,7)], vertPos[random.randint(0,7)], 1)) #BATTLESHIP
+             vshloc.append((horiPos[random.randint(0,7)], vertPos[random.randint(0,7)], 2))  #d                   
+             vshloc.append((horiPos[random.randint(0,7)], vertPos[random.randint(0,7)], 3)) #d
+        while i < len(hshloc):                                    
             j = 0
-            while j <= shloc[i][2]:
-                shp = Ship((shloc[i][0] + (j * (2.5*tx))), shloc[i][1], tx, ty, pygame.image.load("Battleships Images/Ships/Battleship_Large.png"))  #Change distance between ships with (j*tx)
-                ship_list.add(shp)
-                j = j + 1
+            while j <= hshloc[i][2]:  #repeat as long as no exceeding index 2 (how many times it tiles)
+                shp = Ship((hshloc[i][0] + (j * (2*tx))), hshloc[i][1], tx, ty, pygame.image.load("Battleships Images/Ships/Battleship_Large.png"))  #Change distance between ships with (j*tx)
+                ship_list.add(shp)        #^move this to            ^here to flip the tiling vertically
+                j = j + 1                 
             i = i + 1
+        i = 0
+        while i < len(vshloc):                                    
+            j = 0
+            while j <= vshloc[i][2]:  #repeat as long as no exceeding index 2 (how many times it tiles)
+                shp = Ship((vshloc[i][0]), vshloc[i][1] + (j * (2*tx)), tx, ty, pygame.image.load("Battleships Images/Ships/Battleship_Large.png"))  #Change distance between ships with (j*tx)
+                ship_list.add(shp)        #^move this to            ^here to flip the tiling vertically
+                j = j + 1                 
+            i = i + 1               
         return ship_list
 
     def empty(level, tx, ty):
@@ -178,7 +199,7 @@ class Level:
         while i < len(emloc):                                    
             j = 0
             while j <= emloc[i][2]:
-                emp = Empty((emloc[i][0] + (j * (2.5*tx))), emloc[i][1], tx, ty, pygame.image.load("Battleships Images/Ships/no_Ship.png"))  #Change distance between ships with (j*tx)
+                emp = Empty((emloc[i][0] + (j * (2*tx))), emloc[i][1], tx, ty, pygame.image.load("Battleships Images/Ships/no_Ship.png"))  #Change distance between ships with (j*tx)
                 empty_list.add(emp)
                 j = j + 1
             i = i + 1
@@ -227,7 +248,11 @@ while running == True:
     ship_list.draw(screen)
     empty_list.draw(screen)
 
-    if player.torpedos == 0:
+    if not ship_list:
+        print("All ships destroyed, you win!")  #For some reason an empty sprite group is considered false
+        running = False
+
+    if player.torpedos == 0:                    #ordered so that if the player runs out of torpedos on the final shot they still win
         print("Game Over. You Lose")
         running = False
 
